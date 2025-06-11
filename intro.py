c8563debadfbff079e23c91e070c4a5c99cc9d23
pawan2505 <pawanaktu2gmail.com>
2025-06-11 16:53:05 +0530

    print

--- intro.py
+++ intro.py
@@ -1,1 +1,27 @@
+
+# single line comment -> #
+
+# multi line comment -> '''     '''
+
+"""
 print("Hello Python Class!")
+
+print("3+5")
+
+"""
+
+# print(3*5)
+
+
+x = 25
+y = "Pawan"
+
+# print(x)
+# print(y)
+
+# print("The value of x : ",x)
+# print("The value of y : ",y)
+
+
+print(f"The value of x : {x}")
+print(f"The value of y : {y}")
