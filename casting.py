dec0ca11d075526779fce5a0b4a321a7e78ad1dc
pawan2505 <pawanaktu2gmail.com>
2025-06-12 17:00:49 +0530

    variable

---
+++ casting.py
@@ -0,0 +1,99 @@
+
+# num = "54"
+
+# print(num)
+# print(type(num))
+
+# number = int(num)
+
+# print(number)
+# print(type(number))
+
+
+# x = str(3)    # x will be '3'
+# y = int(3)    # y will be 3
+# z = float(3)  # z will be 3.0
+
+
+# x = 23
+
+# print(type(x))
+
+# print(str(x))
+# print(type(str(x)))
+
+# print(float(x))
+# print(type(float(x)))
+
+
+# a = 4
+# A = "Sally"
+#A will not overwrite a
+
+
+# a = 10
+
+# print(a) # 10
+
+# a = "Pawan"
+
+# print(a) # Pawan
+
+# Case-Sensitive
+
+# a = 10
+
+# A = "Pawan"
+
+# print(a) # 10
+# 1a = 10  #not ok
+
+# _a = 10
+
+# print(_a) # 10
+# $a = 10 # not ok
+
+# a1 = 10 # ok
+# keyword we can not use as variable name
+# https://www.w3schools.com/python/python_ref_keywords.asp
+# if = 20 # not ok
+
+# print(a1) # 10
+
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+
+
+# fruits = ["apple", "banana", "cherry"]
+# x,y,z = fruits
+# print(fruits)
+# print(x)
+# print(y)
+# print(z)
+
+
+# x = "Python"
+# y = "is"
+# z = "awesome"
+# print(x, y, z)
+
+
+# x = "5"
+# y = "6"
+# z = "awesome"
+# print(x + y + z)
+
+# x = 5
+# y = "John"
+# print(x + y)
+
+x = 5
+y = "John"
+print(x, y)
