188b2dd0e2089243e317dffd0f9c776b89d0f4b1
pawan2505 <pawanaktu2gmail.com>
2025-06-25 16:51:02 +0530

    variable

---
+++ Revision/variablename.py
@@ -0,0 +1,55 @@
+
+#  rule 1 : we can not use number before variable
+
+# 12num = 10 # not okey
+
+# rule 2 : we can use number after variable name
+
+# num12 = 20 # ok
+
+# rule 3: we can not use white space between variable name
+
+# first name = 100 # not okey
+
+# rule 4 : we can not use reserved keyword as variable name
+
+# if = 20 # not ok
+
+# else = 30 # not ok
+
+# a = 30
+
+# print(a)
+
+# rule 5 : we can not use special character with variable name except _ (underscore)
+
+# _num = 10 # ok
+
+# @num = 10 # not ok
+
+# $num = 10 # not ok
+
+# rule 6 : variable are case sensitive
+
+
+# num =10
+# num = 20 # re-inisilation
+
+# print(Num)
+# print(num)
+
+# rule 7 : variable name can be short or long but it should be meaningfull.
+
+
+# print("Hello","world", sep="-")
+
+# print("Pawan",end="!")
+
+# num = 10
+
+
+#  ctrl + /
+
+# print("\n",num)
+# print(type(num))
+# print(id(num))
